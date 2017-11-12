2130f9de01aac04705cb28450ae6edfb366e01da
GabrieleMessina <gabryelemessina99@gmail.com>
2017-11-12 22:52:31 +0100

    Step implementation

--- src/App.js
+++ src/App.js
@@ -8,21 +8,61 @@ import MenuIcon from 'material-ui/svg-icons/navigation/menu';
 import UploadIcon from 'material-ui/svg-icons/file/file-upload';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import AvatarEditor from 'react-avatar-editor';
-import {RaisedButton, FloatingActionButton ,AppBar, Drawer, IconMenu, MenuItem, IconButton, FlatButton, Toggle} from '../node_modules/material-ui';
+import {Step, 
+        Stepper, 
+        StepLabel,
+        RaisedButton, 
+        FloatingActionButton,
+        AppBar, 
+        Drawer, 
+        IconMenu, 
+        MenuItem, 
+        IconButton, 
+        FlatButton, 
+        Toggle
+}from '../node_modules/material-ui';
+import { __esModule } from 'recompose/pure';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {wtm:true, colorSet:true, open: false, canvasUrl: "http://placehold.it/800x1200", imageUrl: "http://placehold.it/800x1200", image:null}; 
+    this.state = {stepIndex: 0, finished: false, wtm:true, colorSet:true, open: false, canvasUrl: "http://placehold.it/800x1200", imageUrl: "http://placehold.it/800x1200", image:null}; 
   }
 
   componentWillMount(nextProps, nextState) {
 
+    var logoWtm = new Image();
+    logoWtm.crossOrigin = 'anonymous';
+    logoWtm.src = './img/logow.svg';
+    this.setState({ logoWtm: logoWtm });
+
+
+    var frame = new Image();
+    frame.crossOrigin = 'anonymous';
+    frame.src = "./img/frame.svg";
+    this.setState({ frame: frame });
   }
+
+  
+  handleNext = () => {
+    const {stepIndex} = this.state;
+    this.setState({
+      stepIndex: stepIndex + 1,
+      finished: stepIndex >= 2,
+    });
+  };
+
+  handlePrev = () => {
+    const {stepIndex} = this.state;
+    if (stepIndex > 0) {
+      this.setState({stepIndex: stepIndex - 1});
+    }
+  };
 
 
   render() {
-
+    const {finished, stepIndex} = this.state;
+    const contentStyle = {margin: '0 16px'};
 
     const Logged = (props) => (
       <IconMenu
@@ -39,6 +79,37 @@ class App extends React.Component {
       </IconMenu>
       
     );
+
+    const Cropper = (props) => (
+      <div className="canvas-panel">
+        <AvatarEditor
+        {...props}
+          ref={setEditorRef}
+          image={this.state.imageUrl}
+          onMouseUp={mouseUp}
+          width={600}
+          // crossOrigin={"Anonymous"}
+          height={600}
+          border={0}
+          color={[255, 255, 255, 0.6]} // RGBA
+          scale={1}
+          rotate={0}
+        />
+      </div>
+    );
+
+  var getStepContent = (stepIndex) => {
+    switch (stepIndex) {
+      case 0:
+        return <Cropper></Cropper>;
+      case 1:
+        return <canvas id="imgCanvas"></canvas>;
+      case 2:
+        return 'This is the bit I really care about!';
+      default:
+        return 'You\'re a long way from home sonny jim!';
+    }
+  }
 
 
     var WtmToggled = (e, toggle) => {
@@ -64,69 +135,54 @@ class App extends React.Component {
       this.editor = editor; 
     }
     
-    var ImageUrl = (e, toggle) => {
-      console.log("back text", toggle);
-    }
+    var cropping;
+
  
     var imgUpload = (e) => {
-      var logoWtm = new Image();
-      logoWtm.width = 600;
-      logoWtm.height = 600;
-      logoWtm.crossOrigin = 'anonymous';
-      logoWtm.src = './src/img/logow.svg'
-      this.setState({ logoWtm: logoWtm });
-
-  
-      var logo = new Image();
-      logo.crossOrigin = 'anonymous';
-      logo.src = "./img/frame.svg";
-      logo.onload = function() {
-        this.setState({ logo: logo });
-        console.log("setlogo");
-      };
-
-
 
       var image = document.getElementById("inputImage").files[0];
       this.setState({ image: image });
-      console.log("image", image);
-      var canvas = document.getElementById("imgCanvas");
-      this.setState({ canvas: canvas });
-      var ctx = canvas.getContext('2d');
-      this.setState({ ctx: ctx });
+      // console.log("image", image);
+      // var canvas = document.getElementById("imgCanvas");
+      // this.setState({ canvas: canvas });
+      // var ctx = canvas.getContext('2d');
+      // this.setState({ ctx: ctx });
       var url = window.URL || window.webkitURL;
       var src = url.createObjectURL(image);
       var img = new Image();
       img.src = src;
-      this.setState({ canvasUrl: canvas.toDataURL() });
+      //this.setState({ canvasUrl: canvas.toDataURL() });
       this.setState({ imageUrl: src });
-      img.onload = function() {
-        canvas.height = canvas.width;
-        ctx.drawImage(img, 0,0,canvas.width,canvas.width);
-        setCanvaState(ctx, canvas, img, true);
-        //mouseUp();
-      }
+      this.setState({ img: img });
+      // img.onload = function() {
+      //   canvas.height = canvas.width;
+      //   ctx.drawImage(img, 0,0,canvas.width,canvas.width);
+      //   setCanvaState(ctx, canvas, img);
+      //   mouseUp();
+      // }
     }
   
-    var setCanvaState = (ctx, canvas, img, first) =>{
+    var setCanvaState = (ctx, canvas, img) =>{
       this.setState({ ctx: ctx });
       this.setState({ canvas: canvas });
       this.setState({ img: img });
-      this.setState({ canvasUrl: canvas.toDataURL() });
+      this.setState({ canvasUrl: canvas.toDataURL()});
     }
 
-    var mouseUp = () =>{
+    var drawCanvas = () =>{
+      console.log("draw")
       var img = this.state.img;
       if(img == null) return;
       
-      var cropper = document.getElementsByClassName("canvas-panel")[0].children[0];
+      //var cropper = document.getElementsByClassName("canvas-panel")[0].children[0];
       var canvas = document.getElementById("imgCanvas");
-      var ctx = canvas.getContext("2d")
+      var ctx = canvas.getContext("2d");
       this.setState({ canvas: canvas });
       this.setState({ ctx: ctx });
       var colorSet = this.state.colorSet;
       var wtm = this.state.wtm;
-      var cropping = this.editor.getCroppingRect();
+      var frame = this.state.frame;
+      var cropping = this.cropping;
       console.log(cropping);
       if (true) {
         canvas.height = canvas.width;
@@ -134,10 +190,10 @@ class App extends React.Component {
       }
       if (colorSet){ 
         canvas.setAttribute("style", "filter: grayscale(100%)");
-        cropper.setAttribute("style", "filter: grayscale(100%)");
+        //cropper.setAttribute("style", "filter: grayscale(100%)");
       }else {
         canvas.setAttribute("style", "");
-        cropper.setAttribute("style", "");
+        //cropper.setAttribute("style", "");
       }
       if (wtm) {
         var logoWtm = this.state.logoWtm;
@@ -146,7 +202,17 @@ class App extends React.Component {
         var new_height2 = logoWtm.height / logoWtm.width * widthWMT;
         ctx.drawImage(logoWtm, 10, 10, widthWMT, new_height2);
       }
-      setCanvaState(ctx, canvas, img, false);
+      console.log(frame.height);
+      var new_height = frame.height / frame.width * canvas.width;
+      ctx.drawImage(frame, 0, canvas.height - new_height, canvas.width, new_height);      
+      //setCanvaState(ctx, canvas, img);
+    }
+
+    var mouseUp = () =>{
+      console.log("muse up")
+      this.cropping = this.editor.getCroppingRect();
+      
+
     }
 
     return (     
@@ -200,26 +266,60 @@ class App extends React.Component {
             style={{marginTop: 16}}
           />
         </Drawer>
-        <canvas id="imgCanvas"></canvas>
-        <div className="canvas-panel">
-            <AvatarEditor
-              ref={setEditorRef}
-              image={this.state.imageUrl}
-              onMouseUp={mouseUp}
-              width={600}
-              // crossOrigin={"Anonymous"}
-              height={600}
-              border={0}
-              color={[255, 255, 255, 0.6]} // RGBA
-              scale={1}
-              rotate={0}
-            />
-        </div>
+        <Stepper activeStep={stepIndex}>
+          <Step>
+            <StepLabel>Crop your image</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>Add some style</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>Download it!</StepLabel>
+          </Step>
+        </Stepper>
+        
         <a href={this.state.canvasUrl} onClick={downloadImg} download="avatar.png" id="download">
           <FloatingActionButton >
             <ContentAdd />
           </FloatingActionButton>
         </a>
+        <a onClick={drawCanvas} download="avatar.png" id="download">
+          <FloatingActionButton >
+            <ContentAdd />
+          </FloatingActionButton>
+        </a>
+        <div style={contentStyle}>
+          {finished ? (
+            <p>
+              <a
+                href="#"
+                onClick={(event) => {
+                  event.preventDefault();
+                  this.setState({stepIndex: 0, finished: false});
+                }}
+              >
+                Click here
+              </a> to reset the example.
+            </p>
+          ) : (
+            <div>
+              {getStepContent(stepIndex)}
+              <div style={{marginTop: 12}}>
+                <FlatButton
+                  label="Back"
+                  disabled={stepIndex === 0}
+                  onClick={this.handlePrev}
+                  style={{marginRight: 12}}
+                  />
+                <RaisedButton
+                  label={stepIndex === 2 ? 'Finish' : 'Next'}
+                  primary={true}
+                  onClick={this.handleNext}
+                  />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
